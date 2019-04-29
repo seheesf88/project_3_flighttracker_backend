47ea9a7b817802958d26923550f4d2824db28283
seheesf88 <seheesf88@gmail.com>
2019-04-29 16:25:38 -0700

    let

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt  = require('bcryptjs');
 
 const User = require('../models/user');
 const Report = require('../models/report');
@@ -20,6 +21,29 @@ router.get('/:id', async(req, res) => {
     })
   }catch(err){
     res.send(err)
+  }
+})
+
+router.put('/:id', async(req, res) => {
+  try{
+    const modifyProfile = {};
+    const password = req.body.password;
+    const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+    modifyProfile.password = hashedPassword;
+    modifyProfile.username = req.body.username;
+    modifyProfile.email = req.body.email;
+
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, modifyProfile, {new:true})
+    res.json({
+      status: 200,
+      data: 'user is updated',
+      updatedUser: updatedUser
+    })
+
+    
+  }catch(err){
+    console.log('put is err');
+    res.json(err)
   }
 })
 
